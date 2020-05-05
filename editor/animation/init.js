427e6445fde1fc80a1492003e4ffda0d98fc7223
Alexander Lyabah <a.lyabah@checkio.org>
2020-05-05 15:53:26 +0300

    ts

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,126 +1,17 @@
 //Dont change it
-requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
-    function (ext, $, TableComponent) {
-
-        var cur_slide = {};
-
-        ext.set_start_game(function (this_e) {
-        });
-
-        ext.set_process_in(function (this_e, data) {
-            cur_slide["in"] = data[0];
-        });
-
-        ext.set_process_out(function (this_e, data) {
-            cur_slide["out"] = data[0];
-        });
-
-        ext.set_process_ext(function (this_e, data) {
-            cur_slide.ext = data;
-            this_e.addAnimationSlide(cur_slide);
-            cur_slide = {};
-        });
-
-        ext.set_process_err(function (this_e, data) {
-            cur_slide['error'] = data[0];
-            this_e.addAnimationSlide(cur_slide);
-            cur_slide = {};
-        });
-
-        ext.set_animate_success_slide(function (this_e, options) {
-            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div></div></div>'));
-            this_e.setAnimationHeight(115);
-        });
-
-        ext.set_animate_slide(function (this_e, data, options) {
-            var $content = $(this_e.setHtmlSlide(ext.get_template('animation'))).find('.animation-content');
-            if (!data) {
-                console.log("data is undefined");
-                return false;
-            }
-
-            var checkioInput = data.in;
-
-            if (data.error) {
-                $content.find('.call').html('Fail: checkio(' + JSON.stringify(checkioInput) + ')');
-                $content.find('.output').html(data.error.replace(/\n/g, ","));
-
-                $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
-                $content.find('.answer').remove();
-                $content.find('.explanation').remove();
-                this_e.setAnimationHeight($content.height() + 60);
-                return false;
-            }
-
-            var rightResult = data.ext["answer"];
-            var userResult = data.out;
-            var result = data.ext["result"];
-            var result_addon = data.ext["result_addon"];
-
-
-            //if you need additional info from tests (if exists)
-            var explanation = data.ext["explanation"];
-
-            $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
-
-            if (!result) {
-                $content.find('.call').html('Fail: checkio(' + JSON.stringify(checkioInput) + ')');
-                $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
-                $content.find('.answer').addClass('error');
-                $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
-            }
-            else {
-                $content.find('.call').html('Pass: checkio(' + JSON.stringify(checkioInput) + ')');
-                $content.find('.answer').remove();
-            }
-
-            if (explanation) {
-                $content.find('.explanation').html(JSON.stringify(explanation));
-            }
-            else {
-                $content.find('.explanation').remove();
-            }
-
-            //Dont change the code before it
-
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
-
-
-            this_e.setAnimationHeight($content.height() + 60);
-
-        });
-
+//Dont change it
+requirejs(['ext_editor_io', 'jquery_190'],
+    function (extIO, $) {
+        
         var $tryit;
-//
-        ext.set_console_process_ret(function (this_e, ret) {
-            $tryit.find(".checkio-result-in").html("Result: " + ret);
+
+        var io = new extIO({
+            multipleArguments: false,
+            functions: {
+                python: 'checkio',
+                js: 'luckyTickets'
+            }
         });
-
-        ext.set_generate_animation_panel(function (this_e) {
-
-            $tryit = $(this_e.setHtmlTryIt(ext.get_template('tryit')));
-
-            $tryit.find('.bn-check').click(function (e) {
-                var $input = $tryit.find(".ticket-input");
-                var data = $input.val();
-                if (isNaN(data)){
-                    $tryit.find(".checkio-result-in").html("Please enter a number");
-                    return false;
-                }
-                this_e.sendToConsoleCheckiO(data);
-                e.stopPropagation();
-                return false;
-            });
-
-        });
-
+        io.start();
     }
 );
